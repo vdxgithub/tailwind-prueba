2b3254a3fc17628f8ada525bf1dfc495eb9543b7
perezantolin986 <perezantolin986@gmail.com>
2024-06-15 13:34:50 +0000

    First version 2.0

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	plugins: [animations],
+}
