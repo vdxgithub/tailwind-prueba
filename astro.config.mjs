7724da5fd7a2c8a5070b47d146e901c3cc68b8e3
antolinnin00 <perezantolin986@gmail.com>
2024-06-19 15:17:00 +0200

    Change the server to cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import tailwindConfigViewer from "astro-tailwind-config-viewer";
+import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), tailwindConfigViewer()]
+  integrations: [tailwind(), tailwindConfigViewer()],
+  output: "server",
+  adapter: cloudflare(),
 });
